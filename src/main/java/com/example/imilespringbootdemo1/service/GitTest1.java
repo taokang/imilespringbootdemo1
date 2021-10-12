eed635658d6477fca9736ca42ecc7505720d14d1
taokang <1435703893@qq.com>
2021-10-12 16:14:23 +0800

    git test2

---
+++ src/main/java/com/example/imilespringbootdemo1/service/GitTest1.java
@@ -0,0 +1,17 @@
+package com.example.imilespringbootdemo1.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @description:
+ * @author: taokang
+ * @createDate: 2021/10/12
+ * @version: 1.0
+ */
+@Service
+public class GitTest1 {
+
+    public void show(){
+        System.out.println("this is a git test");
+    }
+}
--- src/test/java/com/example/imilespringbootdemo1/Junit4Test1.java
+++ src/test/java/com/example/imilespringbootdemo1/Junit4Test1.java
@@ -1,8 +1,10 @@
 package com.example.imilespringbootdemo1;
 
 
+import com.example.imilespringbootdemo1.service.GitTest1;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,8 +18,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Junit4Test1 {
 
+    @Autowired
+    private GitTest1 gitTest1;
+
     @Test
     public void show(){
         System.out.println("this is a test");
+        gitTest1.show();
     }
 }
